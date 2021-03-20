9cffc2d4a520b33a1465fd990be20e7f64f254ff
xix22 <b.danib@hotmail.com>
2021-03-20 12:43:34 +0100

    Update graphics

--- pygame_demo.py
+++ pygame_demo.py
@@ -11,15 +11,18 @@ gameDisplay = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
 # white = (255, 255, 255)
 
 clock = pygame.time.Clock()
-crashed = False
+stopped = False
+
 CAN_MESSAGES_POS = (350, 450)
 CAN_MESSAGES_DIM = (500, 300)
 
+LAP_INFO_POS = (470, 100)
+
 SPEED_POS = (0, 250)
-POWER_OUTPUT_POS = (100, 450)
+POWER_OUTPUT_POS = (100, 500)
 BATT_CHG_POS = (800, 250)
-BATT_TMP_POS = (950, 450)
-LAP_INFO_POS = (400, 50)
+BATT_TMP_POS = (950, 500)
+
 
 BATT_CHG_COL = pygame.Color('white')
 BATT_TMP_COL = pygame.Color('orange')
@@ -36,12 +39,14 @@ SPEED_FONT = pygame.font.SysFont("lucida console", 178)
 SPEED_UNIT_FONT = pygame.font.SysFont("lucida sans", 42)
 POWER_OUTPUT_FONT = pygame.font.SysFont("lucida console", 75)
 POWER_OUTPUT_UNIT_FONT = pygame.font.SysFont("lucida sans", 38)
-LAP_INFO_FONT = pygame.font.SysFont("lucida sans", 42)
+LAP_INFO_FONT = pygame.font.SysFont("lucida sans", 45)
 CAN_MSG_FONT = pygame.font.SysFont("leelawadee ui", 18)
 
 CAN_MESSAGES_LINE_SPACING = 22
 CAN_MESSAGES_PADDING = 5
 MAX_CAN_MESSAGE_COUNT = 13
+
+LAP_INFO_SPACING = 55
 
 
 class Fps:
@@ -60,9 +65,11 @@ class LapCounter:
         self.total_laps = 21
         self.current_lap = 8
 
+
 class CanDisplay:
     """collects CAN bus messages
     newer messages are at lower index"""
+
     def __init__(self, max_message_count: int):
         self.messages = []
         self.max_message_count = max_message_count
@@ -82,14 +89,13 @@ class CanDisplay:
 fps_counter = Fps()
 lap_counter = LapCounter()
 can_display = CanDisplay(MAX_CAN_MESSAGE_COUNT)
-can_display.insert("Powerup")
+can_display.insert("Power-up")
 can_display.insert("Ready to drive mode active")
-for _ in range(20):
-    can_display.insert("V1")
-while not crashed:
+
+while not stopped:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            crashed = True
+            stopped = True
     gameDisplay.fill((0, 10, 10))
 
     # battery charge
@@ -99,7 +105,8 @@ while not crashed:
 
     # battery temperature
     battery_temperature = 124
-    gameDisplay.blit(BATT_TMP_FONT.render(str(int(battery_temperature)).rjust(3, " "), True, BATT_TMP_COL), BATT_TMP_POS)
+    gameDisplay.blit(BATT_TMP_FONT.render(str(int(battery_temperature)).rjust(3, " "), True, BATT_TMP_COL),
+                     BATT_TMP_POS)
     gameDisplay.blit(BATT_TMP_UNIT_FONT.render("°C", True, BATT_TMP_COL), (BATT_TMP_POS[0] + 145, BATT_TMP_POS[1] + 20))
 
     speed = 43
@@ -108,36 +115,37 @@ while not crashed:
 
     # power output
     power_output = 5
-    gameDisplay.blit(POWER_OUTPUT_FONT.render(str(int(power_output)).rjust(3, " "), True, POWER_OUTPUT_COL), POWER_OUTPUT_POS)
+    gameDisplay.blit(POWER_OUTPUT_FONT.render(str(int(power_output)).rjust(3, " "), True, POWER_OUTPUT_COL),
+                     POWER_OUTPUT_POS)
     gameDisplay.blit(POWER_OUTPUT_UNIT_FONT.render("kW", True, POWER_OUTPUT_COL),
                      (POWER_OUTPUT_POS[0] + 138, POWER_OUTPUT_POS[1] + 21))
 
     # lap info
-    LAP_INFO_SPACING = 50
-    gameDisplay.blit(LAP_INFO_FONT.render(str(lap_counter.current_lap).rjust(2, " ") + " / " + str(lap_counter.total_laps) + " laps",
-                                          True, LAP_INFO_COL), LAP_INFO_POS)
+    gameDisplay.blit(
+        LAP_INFO_FONT.render(str(lap_counter.current_lap).rjust(2, " ") + " / " + str(lap_counter.total_laps) + " laps",
+                             True, LAP_INFO_COL), LAP_INFO_POS)
     power_consumption_feedback_average = -2
     power_consumption_feedback_average_text = ("+" * (power_consumption_feedback_average >= 1)) + \
                                               str(int(power_consumption_feedback_average))
     gameDisplay.blit(LAP_INFO_FONT.render("AVG   " + power_consumption_feedback_average_text + "%",
-                                          True, LAP_INFO_COL), (LAP_INFO_POS[0], LAP_INFO_POS[1]+LAP_INFO_SPACING))
+                                          True, LAP_INFO_COL), (LAP_INFO_POS[0], LAP_INFO_POS[1] + LAP_INFO_SPACING))
 
     power_consumption_feedback_total = 3
     power_consumption_feedback_total_text = ("+" * (power_consumption_feedback_total >= 1)) + \
                                             str(int(power_consumption_feedback_total))
     gameDisplay.blit(LAP_INFO_FONT.render("LAST " + power_consumption_feedback_total_text + "%",
-                                          True, LAP_INFO_COL), (LAP_INFO_POS[0], LAP_INFO_POS[1]+LAP_INFO_SPACING*2))
+                                          True, LAP_INFO_COL),
+                     (LAP_INFO_POS[0], LAP_INFO_POS[1] + LAP_INFO_SPACING * 2))
 
     # CAN messages
     r = pygame.rect.Rect(*CAN_MESSAGES_POS, *CAN_MESSAGES_DIM)
     pygame.draw.rect(gameDisplay, pygame.Color("white"), r, 1)
 
-
     for i, m in enumerate(can_display.get_messages()):
         gameDisplay.blit(CAN_MSG_FONT.render(m, True, pygame.Color('white')),
                          (CAN_MESSAGES_POS[0] + CAN_MESSAGES_PADDING,
                           CAN_MESSAGES_POS[1] + CAN_MESSAGES_DIM[1] -
-                          CAN_MESSAGES_LINE_SPACING * i - CAN_MESSAGES_PADDING-30))
+                          CAN_MESSAGES_LINE_SPACING * i - CAN_MESSAGES_PADDING - 30))
 
     gameDisplay.blit(fps_counter.update(), (10, 0))
 
